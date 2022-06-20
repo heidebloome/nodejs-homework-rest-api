9d53975458e62e7a7e1484435540ea49cd9fe8c8
Vira Velmozhna <viravelmozhna@gmail.com>
2022-06-20 22:42:42 +0200

    functions for getting, updating and deleting contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,84 @@
-const express = require('express')
+const express = require('express');
+const Joi = require("joi");
+
+const contactsOperations = require("../../models/contacts");
+const createError = require("../../utils");
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const newContactSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().required(),
 })
+
+router.get('/', async (req, res, next) => {
+  try {
+    const contacts = await contactsOperations.listContacts();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  };
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const contact = await contactsOperations.getContactById(contactId);
+    if (!contact) {
+      console.log(createError);
+      throw createError(404);
+    };
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  };
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = newContactSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const newContact = await contactsOperations.addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const removedContact = await contactsOperations.removeContact(contactId);
+    if (!removedContact) {
+      throw createError(404);
+    };
+    res.json({
+      message: "contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = newContactSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    };
+    const { contactId } = req.params;
+    const updatedContact = await contactsOperations.updateContact(contactId, req.body);
+    console.log(updatedContact);
+    if (!updatedContact) {
+      throw createError(404);
+    };
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
 })
 
 module.exports = router
---
+++ utils/createError.js
@@ -0,0 +1,15 @@
+const errorMessages = {
+  400: "Bad request",
+  401: "Not authorized",
+  403: "Forbidden",
+  404: "Not found",
+}
+const createError = (status, message = errorMessages[status]) => {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+}
+
+module.exports = {
+  createError,
+};
